c77b72be18921876f10b765999d445daef3b729e
MalmarPadecain <malmar.padecain@gmail.com>
2019-05-30 23:35:27 +0200

    moved to typescript

---
+++ jukebox.ts
@@ -0,0 +1,67 @@
+class Playlist {
+    name: String;
+    songs: Array<Song>;
+}
+
+class Song {
+    name: String;
+    link: String;
+    duration: String;
+}
+
+class State {
+    audio: HTMLAudioElement;
+    shuffle: Boolean;
+    playlist: Playlist;
+}
+
+const state = new State;
+
+function generateTable() { //probably needs to be rewritten. looks ugly
+    let path = "/Jukebox/playlists/EVE_Soundtrack.json";
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            let result = xhr.responseText;
+            state.playlist = <Playlist>JSON.parse(result);
+            let table = document.getElementById("table");
+            for (let song of state.playlist.songs) {
+                table.innerHTML += `<tr onclick="play('${song.link}')"><td>num</td><td>${song.name}</td><td>${song.duration}</td></tr>`;
+            }
+        }
+    };
+    xhr.open("GET", path, true);
+    xhr.send();
+}
+
+function play(link) {
+    state.audio.src = link;
+    state.audio.load();
+    state.audio.play()
+}
+
+function updateVolume() {
+    let volumeControl = <HTMLInputElement>document.getElementById("volume");
+
+    state.audio.volume = parseFloat(volumeControl.value);
+}
+
+function updateProgress() {
+    let progress = document.getElementById("progress");
+    progress.innerText = (Math.round(state.audio.currentTime / state.audio.duration * 100)).toString() + "%";
+}
+
+function togglePause() {
+    if (state.audio.paused) {
+        state.audio.play();
+    } else {
+        audio.pause();
+    }
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    state.audio = <HTMLAudioElement>document.getElementById("audio");
+    state.shuffle = false;
+    generateTable();
+});
+setInterval(updateProgress, 1000);
