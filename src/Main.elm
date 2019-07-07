713e9acb4ba2762673f1cb023c2dc803688cd05f
MalmarPadecain <malmar.padecain@gmail.com>
2019-07-07 16:28:52 +0200

    Load Playlists from JSON files

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,11 @@
-module Main exposing (main)
+module Main exposing (..)
 
-import Array
+import Array exposing (Array)
 import Browser
 import Html exposing (Html, button, div, li, text, ul)
 import Html.Events exposing (onClick)
+import Http
+import Json.Decode exposing (Decoder, field, string, array, map2, map3)
 import Maybe
 import Random exposing (generate)
 import Random.Array exposing (shuffle)
@@ -16,33 +18,29 @@ type alias Song =
     }
 
 
-type alias Playlist = Array.Array Song
+type alias Playlist =
+    { name : String
+    , playlist : Array Song}
 
 
-type alias Model =
-    { playlist : Playlist
+type Model =
+    Success { playlist : Playlist
     , index : Int
-    }
+    } | Error String
 
 
 type Msg
     = Next
     | Previous
     | Shuffle
-    | Shuffled (Array.Array Song)
-
-
-songList : Playlist
-songList =
-    Array.fromList [ { name = "Unidentified Phenomenon"
-    , duration = "4:30"
-    , link = "https://www.modenstudios.com/EVE/music/Unidentified%20Phenomenon.mp3"}
-    ]
+    | Shuffled (Array Song)
+    | Load String
+    | Loaded (Result Http.Error Playlist)
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( { playlist = songList
+    ( Success { playlist = { name = "Empty", playlist = Array.empty }
       , index = 0
       }
     , Cmd.none
@@ -62,41 +60,75 @@ update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Next ->
-            if model.index == Array.length model.playlist - 1 then
-                ( { playlist = model.playlist, index = 0 }, Cmd.batch [] )
-
-            else
-                ( { playlist = model.playlist, index = model.index + 1 }, Cmd.batch [] )
-
+            case model of
+                Success playlist ->
+                    if playlist.index == Array.length playlist.playlist.playlist - 1
+                    then ( Success { playlist = playlist.playlist, index = 0 }, Cmd.batch [] )
+                    else( Success { playlist = playlist.playlist, index = playlist.index + 1 }, Cmd.batch [] )
+                Error _ ->
+                    (model, Cmd.batch [])
         Previous ->
-            if model.index == 0 then
-                ( { playlist = model.playlist, index = Array.length model.playlist - 1 }, Cmd.batch [] )
+            case model of
+                Success playlist ->
+                    if playlist.index == 0 then
+                        ( Success{ playlist = playlist.playlist, index = Array.length playlist.playlist.playlist - 1 }, Cmd.batch [] )
 
-            else
-                ( { playlist = model.playlist, index = model.index - 1 }, Cmd.batch [] )
-
+                    else
+                        ( Success{ playlist = playlist.playlist, index = playlist.index - 1 }, Cmd.batch [] )
+                Error _ ->
+                    (model, Cmd.batch [])
         Shuffle ->
-            ( model, generate Shuffled (shuffle model.playlist) )
-
+            case model of
+                Success playlist ->
+                    ( model, generate Shuffled (shuffle playlist.playlist.playlist) )
+                Error _ ->
+                    (model, Cmd.batch [])
         Shuffled shuffledList ->
-            ( { playlist = shuffledList, index = 0 }, Cmd.batch [] )
+            ( Success{ playlist = { name = "", playlist = shuffledList } , index = 0 }, Cmd.batch [] )
+        Load link ->
+            ( model, loadJson link)
+        Loaded result ->
+            case result of
+                Ok lst ->
+                    (Success { playlist = lst , index = 0}, Cmd.none)
+                Err err ->
+                    case err of
+                        Http.BadUrl url ->
+                            (Error ("Bad URL: " ++ url), Cmd.none)
+                        Http.Timeout ->
+                            (Error ("Request timed out."), Cmd.none)
+                        Http.NetworkError ->
+                            (Error ("A Network Error occurred"), Cmd.none)
+                        Http.BadStatus status ->
+                            (Error (String.fromInt status ++ " returned"), Cmd.none)
+                        Http.BadBody body ->
+                            (Error ("Bad Body: " ++ body ), Cmd.none)
 
 
 renderList : Model -> Html msg
 renderList model =
-    model.playlist
-        |> Array.toList
-        |> List.map (\l -> li [] [ text l.name ])
-        |> ul []
+    case model of
+        Success playlist ->
+            playlist.playlist.playlist
+                |> Array.toList
+                |> List.map (\l -> li [] [ text l.name ])
+                |> ul []
+        Error err ->
+            div [] [ text err ]
 
 
 currentlyPlaying : Model -> Song
 currentlyPlaying model =
-    case Array.get model.index model.playlist of
-        Nothing ->
+    case model of
+        Success playlist ->
+            case Array.get playlist.index playlist.playlist.playlist of
+                Nothing ->
+                    {name="", link="", duration=""}
+                Just song ->
+                    song
+        Error _ ->
             {name="", link="", duration=""}
-        Just song ->
-            song
+
 
 view : Model -> Browser.Document Msg
 view model =
@@ -105,7 +137,30 @@ view model =
         [ button [ onClick Shuffle ] [ text "Shuffle" ]
         , button [ onClick Previous ] [ text "-" ]
         , renderList model
-        , div [] [ text ((currentlyPlaying model).name ++ " " ++ String.fromInt model.index) ]
+        , div [] [ text ((currentlyPlaying model).name) ]
         , button [ onClick Next ] [ text "+" ]
+        , button [ onClick (Load "playlists/EVE_Soundtrack.json") ] [text "Load"]
         ]
     }
+
+
+--viewPlaylist : Model -> Html Msg
+--viewPlaylist model =
+
+
+loadJson : String -> Cmd Msg
+loadJson link =
+    Http.get
+        { url = link
+        , expect = Http.expectJson Loaded songDecoder}
+
+
+songDecoder : Decoder Playlist
+songDecoder =
+    map2 Playlist
+        (field "name" string)
+        (field "songs"
+            (array (map3 Song
+                 (field "name" string)
+                 (field "link" string)
+                 (field "duration" string))))
