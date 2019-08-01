74c6cc0f6eee786c95a2025042242b4b4f361057
MalmarPadecain <malmar.padecain@gmail.com>
2019-08-01 16:48:51 +0200

    updated view

--- src/View.elm
+++ src/View.elm
@@ -4,8 +4,8 @@ import Array
 import Browser
 import Core exposing (..)
 import Draggable
-import Html exposing (Html, a, audio, br, button, div, h1, hr, img, input, p, table, tbody, td, text, th, tr)
-import Html.Attributes exposing (class, href, id, max, min, src, step, style, title, type_, value)
+import Html exposing (Html, a, audio, b, br, button, div, h1, hr, img, input, p, source, table, tbody, td, text, th, tr, ul, video)
+import Html.Attributes exposing (autoplay, class, href, id, loop, max, min, src, step, style, title, type_, value)
 import Html.Events exposing (on, onClick, onInput)
 import Html.Lazy exposing (lazy)
 import Json.Decode as Decode
@@ -47,28 +47,27 @@ view model =
                         ]
                     , div [ class "jukeboxMain", id "buttonList" ]
                         [ div [ class "controlBtnContainer" ]
-                            [ img [ class "controlBtn", src "images/btnBack.jpg", onClick Previous ]
+                            [ div [ class "controlBtn", id "btnBack", onClick Previous ]
                                 []
-                            , img
+                            , div
                                 [ class "controlBtn"
                                 , if playlist.playing then
-                                    src "images/btnPause.jpg"
+                                    id "btnPause"
 
                                   else
-                                    src "images/btnPlay.jpg"
+                                    id "btnPlay"
                                 , onClick TogglePause
                                 ]
                                 []
-                            , img [ class "controlBtn", src "images/btnFwd.jpg", onClick Next ]
+                            , div [ class "controlBtn", id "btnFwd", onClick Next ]
                                 []
-                            , img
+                            , div
                                 [ class "controlBtn"
-                                , id "btnShuffle"
                                 , if playlist.shuffledSongs == Nothing then
-                                    src "images/btnShuffleOff.jpg"
+                                    id "btnShuffleOff"
 
                                   else
-                                    src "images/btnShuffleOn.jpg"
+                                    id "btnShuffleOn"
                                 , onClick Shuffle
                                 ]
                                 []
@@ -139,12 +138,14 @@ view model =
                         [ div [ class "NewBtnContainer" ]
                             [ div [ id "newBtn" ]
                                 [ button [ class "EVEButton" ]
-                                    [ text "New" ]
+                                    [ text "Opt" ]
                                 ]
                             ]
                         , div [ class "AddBtnContainer" ]
                             [ a [ class "EVEButton", href "#open-modal" ]
-                                [ text "About" ]
+                                [ button [ class "EVEButton" ]
+                                    [ text "About" ]
+                                ]
                             ]
                         ]
                     , aboutWindow
@@ -154,6 +155,7 @@ view model =
                         ]
                         []
                     ]
+                , videoDiv
                 ]
             }
 
@@ -246,24 +248,83 @@ aboutWindow =
     div [ class "modal-window", id "open-modal" ]
         [ div []
             [ a [ class "modal-close", href "#modal-close", title "Close" ]
-                [ text "CLOSE" ]
+                [ text "CLOSE ×" ]
             , h1 [ class "modalText" ]
                 [ text "The EVE Online Jukebox Project" ]
             , div [ class "modalText" ]
-                [ hr [] []
+                [ hr []
+                    []
                 , p []
-                    [ text "Bring back the good old times of EVE with this recreation of the EVE Jukebox using CSS." ]
-                , br [] []
-                , p [ id "smallText" ]
-                    [ text """EVE Online and the EVE logo are the registered trademarks of CCP hf.
-                               All rights are reserved worldwide. All other trademarks are the property of their
-                               respective owners. EVE Online, the EVE logo, EVE and all associated logos and designs
-                               are the intellectual property of CCP hf. All artwork, screenshots, characters, vehicles,
-                               storylines, world facts or other recognizable features of the intellectual property
-                               relating to these trademarks are likewise the intellectual property of CCP hf.
-                               CCP is in no way responsible for the content on or functioning of this website, nor can
-                               it be liable for any damage arising from the use of this website.""" ]
-                , br [] []
+                    [ text "Bring back the good old times of EVE with this recreation of the EVE Jukebox using CSS. If you find any problems please let us know on Discord at Laura#1479 or Malmar Padecain#0659        " ]
+                , div [ class "playlistDescriptionContainer" ]
+                    [ div [ class "playlistDescription" ]
+                        [ ul []
+                            [ b [ class "orangeText" ]
+                                [ text "EVE Soundtrack:" ]
+                            , text "The original soundtrack as it was in the Crucible jukebox in 2011."
+                            ]
+                        , ul []
+                            [ b [ class "orangeText" ]
+                                [ text "Mission Music:" ]
+                            , text "All of the music found in missions across New Eden. Mostly rock music."
+                            ]
+                        , ul []
+                            [ b [ class "orangeText" ]
+                                [ text "Login Themes:" ]
+                            , text "A chronological list of expansion themes, or \"login music\"."
+                            ]
+                        , ul []
+                            [ b [ class "orangeText" ]
+                                [ text "Cinematic Themes:" ]
+                            , text "Music from trailers."
+                            ]
+                        , ul []
+                            [ b [ class "orangeText" ]
+                                [ text "Permaband:" ]
+                            , text "Only the best band this side of Jita."
+                            ]
+                        ]
+                    , div [ class "playlistDescription" ]
+                        [ ul []
+                            [ b [ class "orangeText" ]
+                                [ text "Misc Tracks:" ]
+                            , text "Various tracks that didn't fit anywhere else."
+                            ]
+                        , ul []
+                            [ b [ class "orangeText" ]
+                                [ text "Upbeat Music:" ]
+                            , text "Our own selection of fast-paced music for combat and such."
+                            ]
+                        , ul []
+                            [ b [ class "orangeText" ]
+                                [ text "Peace Logs:" ]
+                            , text "Fan-made ambient and downtempo music."
+                            ]
+                        , ul []
+                            [ b [ class "orangeText" ]
+                                [ text "War Logs:" ]
+                            , text "Fan made drum and bass or dubstep."
+                            ]
+                        , ul []
+                            [ b [ class "orangeText" ]
+                                [ text "Dead Logs:" ]
+                            , text "Fan made dark ambient/drone music."
+                            ]
+                        ]
+                    ]
+                , hr []
+                    []
+                , br []
+                    []
+                , p [ class "smallText" ]
+                    [ text "EVE Online and the EVE logo are the registered trademarks of CCP hf. All rights are reserved worldwide. All other trademarks are the property of their respective owners. EVE Online, the EVE logo, EVE and all associated logos and designs are the intellectual          property of CCP hf. All artwork, screenshots, characters, vehicles, storylines, world facts or other recognizable features of the intellectual property relating to these trademarks are likewise the intellectual property of CCP hf. CCP is in no way          responsible for the content on or functioning of this website, nor can it be liable for any damage arising from the use of this website.        " ]
+                , p [ class "smallText" ]
+                    [ text "Most of the music here is property of CCP Games and can be better accessed in a more modern interface at their "
+                    , a [ class "orangeText", href "https://soundcloud.com/ccpgames" ]
+                        [ text "Soundcloud" ]
+                    ]
+                , br []
+                    []
                 , p [ id "stylingBy" ]
                     [ a [ href "https://ashyin.space/", id "stylingBy" ]
                         [ text "Styling by Ashley Traynor" ]
@@ -273,10 +334,19 @@ aboutWindow =
                         [ text "Backend by Malmar Padecain" ]
                     , br []
                         []
-                    , a [ href "https://github.com/MalmarPadecain/EVE_Jukebox", id "gitLink" ]
+                    , a [ class "orangeText", href "https://github.com/MalmarPadecain/EVE_Jukebox" ]
                         [ text "This is an open source project!" ]
                     ]
                 ]
+            ]
+        ]
+
+
+videoDiv : Html msg
+videoDiv =
+    div [ id "videoWrapper" ]
+        [ video [ id "film", style "z-index" "8", autoplay True, loop True ]
+            [ source [ src "video/minmatarStation.mp4", type_ "video/mp4" ] []
             ]
         ]
 
