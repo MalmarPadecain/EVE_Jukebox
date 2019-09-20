4503fbc9c2880980169b664a2599e12fa62352ea
MalmarPadecain <malmar.padecain@gmail.com>
2019-09-20 17:47:28 +0200

    now also builds a minimal version without video, image and music data

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 
 elm make src/Main.elm --optimize --output=main.js
 
-tar -czf Jukebox.tar.gz jukebox.html src/controls.js main.js playlists/ css images video music LICENSE README.md
+tar -czf Jukebox_full.tar.gz jukebox.html src/controls.js main.js playlists/ css images video music LICENSE README.md
+tar -czf Jukebox.tar.gz jukebox.html src/controls.js main.js playlists/ css LICENSE README.md
